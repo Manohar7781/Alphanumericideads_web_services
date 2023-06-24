0a8fcbad597ae85ac325ba541699f109147b6ea5
BALASARVAN12 <91976828+BALASARVAN12@users.noreply.github.com>
2023-06-24 21:48:55 +0530

    added contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import "../Css/Contact.css"
+import "../Css/Contact.css";
+import contact_banner from "../assests/2.jpg"
+import Hero from './Hero';
 
 export default function Contact() {
   return (
-    <div className='form-container'>
-      <h1>Send Message To Us</h1>
-      <form action="">
-        <label htmlFor=""></label>
-      </form>
+    <div>
+      <Hero
+      cName = "hero"
+      heroImg = {contact_banner}
+      title="Contact Us"
+      text="Thank you for your intrest in our Alphanumeric Ideas Services
+      Contact us today to learn more about how we can assist you in maximizing your productivity and efficiency.
+      "/>
+      <br />
     </div>
   )
 }
